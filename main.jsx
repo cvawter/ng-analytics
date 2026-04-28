55b5d1b82a2afba968ee793e8321c2751b4beb31
Chad Vawter <chad.vawter@gmail.com>
2026-04-28 16:13:37 -0400

    Created React app structure with Antigravity

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EIANaturalGasReports from './ngreports.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EIANaturalGasReports />
+  </React.StrictMode>,
+)
